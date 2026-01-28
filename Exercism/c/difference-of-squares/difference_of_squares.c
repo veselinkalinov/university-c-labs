53ab98fdfd327fe45cfbae686b3398af371a8d0c
Veselin <vesko.kalinov06@gmail.com>
2026-01-28 11:15:42 +0000

    Jan 28, 2026, 12:15 PM

--- Exercism/c/difference-of-squares/difference_of_squares.c
+++ Exercism/c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,19 @@
 #include "difference_of_squares.h"
+
+unsigned int square_of_sum(unsigned int number)
+{
+    unsigned long long sum = (unsigned long long)number * (number + 1) / 2;
+    return (unsigned int)(sum * sum);
+}
+
+unsigned int sum_of_squares(unsigned int number)
+{
+    unsigned long long result = (unsigned long long)number * (number + 1) * (2 * number + 1) / 6;
+    return (unsigned int)result;
+}
+
+unsigned int difference_of_squares(unsigned int number)
+{
+    return square_of_sum(number) - sum_of_squares(number);
+}
+#include "difference_of_squares.h"
