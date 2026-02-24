07ca2bc9f6072360d6527723e9fe401cbb3ded51
veselinkalinov <vesko.kalinov06@gmail.com>
2026-02-24 16:24:23 +0200

    commit

--- Lekcii/Lecture1.c
+++ Lekcii/Lecture1.c
@@ -1,97 +1,82 @@
-#include <stdio.h>
 
-int main()
-{
-    // --- Types of data ---
-    int x = 52;              // Whole numbers (integer)
-    double y = 6.4715156132; // Real numbers (64bit)
-    float z = 4.922;         // Real numbers (32bit)
-    char a = 'B';            // Characters
 
-    // --- If Else statements ---
-    if (x > y)
-    {
-        // code;
+int main() {
+  // --- Types of data ---
+  int x = 52;              // Whole numbers (integer)
+  double y = 6.4715156132; // Real numbers (64bit)
+  float z = 4.922;         // Real numbers (32bit)
+  char a = 'B';            // Characters
+
+  // --- If Else statements ---
+  if (x > y) {
+    // code;
+  } else {
+    // code;
+  }
+
+  // --- Switch-Case statements ---
+  switch (x) {
+  case 1:
+    // code;
+    break;
+  case 2:
+    // code;
+    break;
+  default:
+    break;
+  }
+
+  // --- For loop ---
+  for (int i = 0; i < 10; i++) {
+    // code;
+  }
+
+  // --- While loop ---
+  while (x > 2) {
+    x++;
+    // code;
+    if (x == 10) {
+      break;
     }
-    else
-    {
-        // code;
+  }
+
+  // --- Do-While loop ---
+  do {
+    // code;
+    x++;
+  } while (x < 10);
+
+  // --- Arrays ---
+  int array1[10];
+  for (int i = 0; i < 10; i++) {
+    array1[i] = i * 2;
+  }
+
+  // Finding the smallest number in an array
+  int min = array1[0];
+  for (int i = 0; i < 10; i++) {
+    if (array1[i] < min) {
+      min = array1[i];
     }
+  }
 
-    // --- Switch-Case statements ---
-    switch (x)
-    {
-    case 1:
-        // code;
-        break;
-    case 2:
-        // code;
-        break;
-    default:
-        break;
+  // Finding the largest number in an array
+  int max = array1[0];
+  for (int i = 0; i < 10; i++) {
+    if (array1[i] > max) {
+      max = array1[i];
     }
+  }
 
-    // --- For loop ---
-    for (int i = 0; i < 10; i++)
-    {
-        // code;
-    }
+  // Finding average
+  int sum = 0;
+  double avarage = 0;
+  for (int i = 0; i < 10; i++) {
+    sum += array1[i];
+    // Забележка: i започва от 0, за коректна средна стойност обикновено се дели
+    // на броя елементи (10)
+    avarage = (double)sum / (i + 1);
+  }
 
-    // --- While loop ---
-    while (x > 2)
-    {
-        x++;
-        // code;
-        if (x == 10)
-        {
-            break;
-        }
-    }
-
-    // --- Do-While loop ---
-    do
-    {
-        // code;
-        x++;
-    } while (x < 10);
-
-    // --- Arrays ---
-    int array1[10];
-    for (int i = 0; i < 10; i++)
-    {
-        array1[i] = i * 2;
-    }
-
-    // Finding the smallest number in an array
-    int min = array1[0];
-    for (int i = 0; i < 10; i++)
-    {
-        if (array1[i] < min)
-        {
-            min = array1[i];
-        }
-    }
-
-    // Finding the largest number in an array
-    int max = array1[0];
-    for (int i = 0; i < 10; i++)
-    {
-        if (array1[i] > max)
-        {
-            max = array1[i];
-        }
-    }
-
-    // Finding average
-    int sum = 0;
-    double avarage = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        sum += array1[i];
-        // Забележка: i започва от 0, за коректна средна стойност обикновено се дели
-        // на броя елементи (10)
-        avarage = (double)sum / (i + 1);
-    }
-
-    return 0;
+  return 0;
 }
