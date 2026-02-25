287e39a8a9302794fe8b0fa7a1271aec1160444c
veselinkalinov <vesko.kalinov06@gmail.com>
2026-02-25 06:36:10 +0000

    Feb 25, 2026, 8:36 AM

--- Seminarni/UPR_1/zad2.c
+++ Seminarni/UPR_1/zad2.c
@@ -11,9 +11,9 @@ int main()
     double dm = inch * 0.254;
     double m = inch * 0.0254;
 
-    printf("%4f inches=\n", inch);
-    printf("%4f mm\n", mm);
-    printf("%4f cm\n", cm);
-    printf("%4f dm\n", dm);
-    printf("%4f m\n", m);
+    printf("%.4lf inches=\n", inch);
+    printf("%.4f mm\n", mm);
+    printf("%.4f cm\n", cm);
+    printf("%.4f dm\n", dm);
+    printf("%.4f m\n", m);
 }
