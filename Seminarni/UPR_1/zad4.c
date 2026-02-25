821e259198eb53cd73c01c68442b9ebfd4524224
veselinkalinov <vesko.kalinov06@gmail.com>
2026-02-25 06:50:09 +0000

    Feb 25, 2026, 8:50 AM

--- Seminarni/UPR_1/zad4.c
+++ Seminarni/UPR_1/zad4.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
+
+int main()
+{
+    double deg;
+    printf("Enter degrees:");
+    scanf("%lf", &deg);
+
+    double rad = deg * (M_PI / 180);
+    printf("%.2lf degrees are %.2f radians", deg, rad);
+    return 0;
+}
