d57e5562428d4dda6e06d737758455f4ef102273
veselinkalinov <vesko.kalinov06@gmail.com>
2026-02-25 06:24:48 +0000

    Feb 25, 2026, 8:24 AM

--- Seminarni/SU_1.c
+++ Seminarni/SU_1.c
@@ -25,7 +25,14 @@ int main()
 // zad 2
 int main()
 {
-    float inch;
+    double inch;
     printf("Inches:");
     scanf("%f", &inch);
+
+    double mm = inch * 25.4;
+    double cm = inch * 2.54;
+    double dm = inch * 0.254;
+    double m = inch * 0.0254
+
+               printf("%4f")
 }
