4318431cc5c0d2626220d78f0e9fa3d18d417308
veselinkalinov <vesko.kalinov06@gmail.com>
2026-02-25 06:42:02 +0000

    Feb 25, 2026, 8:42 AM

--- Seminarni/UPR_1/zad3.c
+++ Seminarni/UPR_1/zad3.c
@@ -5,4 +5,7 @@ int main()
     double deg;
     printf("Enter degrees:");
     scanf("%lf", &deg);
+
+    double far = (deg * 1.8) + 32;
+    printf("%.2f Celsius are %.2f in Fahrenheit", deg, far);
 }
