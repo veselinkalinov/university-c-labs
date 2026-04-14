d1564cb592e7a2e632f47a43b96b880da7bd4d0a
veselinkalinov <vesko.kalinov06@gmail.com>
2026-04-14 14:35:37 +0000

    Apr 14, 2026, 5:35 PM

--- Laboratorni/Lab 7 - 14.04.2026/task_1.c
+++ Laboratorni/Lab 7 - 14.04.2026/task_1.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#define SIZE 5
 
 void reverseArr(int *arr, int size);
 
-int main(void) { return 0; }
+int main(void) {
+  int arr[SIZE] = {1, 2, 3, 4, 5};
 
-void reverseArr(int *arr, int size) { int start = 0, end = size, temp; }
+  return 0;
+}
+
+void reverseArr(int *arr, int size) {
+  int start = 0, end = size - 1, temp;
+  for (; size <= end; end--) {
+    temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+  }
+}
